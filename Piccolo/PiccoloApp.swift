3854caf1c88175ee5e83c2fc7b0a2073d4dc6997
Bronek Szulc <bronek@fastmail.fm>
2024-11-16 15:47:36 -0500

    scaffold
    add swift ui

---
+++ Piccolo/PiccoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiccoloApp.swift
+//  Piccolo
+//
+//  Created by Bronek on 2024-11-16.
+//
+
+import SwiftUI
+
+@main
+struct PiccoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PiccoloTests/PiccoloTests.swift
@@ -0,0 +1,17 @@
+//
+//  PiccoloTests.swift
+//  PiccoloTests
+//
+//  Created by Bronek on 2024-11-16.
+//
+
+import Testing
+@testable import Piccolo
+
+struct PiccoloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
